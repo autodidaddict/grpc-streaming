41566771c9a161c81800ec518442699ff593c1be
Kevin Hoffman <alothien@gmail.com>
2018-09-12 08:56:08 -0400

    Adding client and server one-way streaming

--- cmd/customerctl/main.go
+++ cmd/customerctl/main.go
@@ -3,23 +3,80 @@ package main
 import (
 	"context"
 	"fmt"
-	"google.golang.org/grpc"
 	"io"
 	"time"
+
 	pb "github.com/autodidaddict/grpc-streaming/proto"
+	"google.golang.org/grpc"
 )
 
 func main() {
 	var opts []grpc.DialOption
 	opts = append(opts, grpc.WithInsecure())
 
-	conn, err := grpc.Dial("127.0.0.1:8088", opts ...)
+	conn, err := grpc.Dial("127.0.0.1:8088", opts...)
 	if err != nil {
 		panic(err)
 	}
 	defer conn.Close()
 	client := pb.NewCustomersClient(conn)
 
+	demoTwoWayStreaming(client)
+	demoServerStreaming(client)
+	demoClientStreaming(client)
+}
+
+func demoClientStreaming(client pb.CustomersClient) {
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	stream, err := client.ImportCustomers(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < 10; i++ {
+		cust := &pb.Customer{
+			CustomerId: fmt.Sprintf("CUST-%d", i),
+			GivenName:  "Random",
+			Surname:    "Entropy",
+		}
+		if err := stream.Send(cust); err != nil {
+			panic(err)
+		}
+		fmt.Println("Sent 1 customer for import")
+	}
+	reply, err := stream.CloseAndRecv()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Imported all customers: %+v\n", reply)
+}
+
+func demoServerStreaming(client pb.CustomersClient) {
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	stream, err := client.GetCustomerOrders(ctx, &pb.CustomerRequest{
+		CustomerId: fmt.Sprintf("CUST-%d", 12),
+	})
+	if err != nil {
+		panic(err)
+	}
+	for {
+		order, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Received order on stream %+v\n", order)
+	}
+	fmt.Println("Downloaded all customer orders")
+}
+
+func demoTwoWayStreaming(client pb.CustomersClient) {
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
@@ -43,7 +100,7 @@ func main() {
 		}
 	}()
 	// Stream SENDER
-	for i :=0; i < 10; i++ {
+	for i := 0; i < 10; i++ {
 		customerID := fmt.Sprintf("CUST-%d", i)
 		req := &pb.CustomerRequest{
 			CustomerId: customerID,
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,20 +1,61 @@
 package handler
 
 import (
-	"github.com/go-kit/kit/log"
-	pb "github.com/autodidaddict/grpc-streaming/proto"
-	"github.com/go-kit/kit/log/level"
+	"fmt"
 	"io"
+	"time"
+
+	pb "github.com/autodidaddict/grpc-streaming/proto"
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
 )
 
 type rpcHandler struct {
 	logger log.Logger
 }
 
+// NewCustomersHandler creates an instance of a gRPC endpoint handler for the customers service
 func NewCustomersHandler(logger log.Logger) pb.CustomersServer {
-	return &rpcHandler {
+	return &rpcHandler{
 		logger: logger,
 	}
+}
+
+func (r *rpcHandler) ImportCustomers(stream pb.Customers_ImportCustomersServer) error {
+	var recordCount int32
+	startTime := time.Now()
+
+	for {
+		customer, err := stream.Recv()
+		if err == io.EOF {
+			endTime := time.Now()
+			return stream.SendAndClose(&pb.ImportSummary{
+				TotalCustomers: recordCount,
+				ErrorCount:     95781,
+				ElapsedTime:    int32(endTime.Sub(startTime).Seconds()),
+			})
+		}
+		if err != nil {
+			return err
+		}
+		level.Info(r.logger).Log("msg", "Received customer import", "customer", customer)
+		recordCount++
+		time.Sleep(500 * time.Millisecond)
+	}
+}
+
+func (r *rpcHandler) GetCustomerOrders(req *pb.CustomerRequest, stream pb.Customers_GetCustomerOrdersServer) error {
+	for i := 0; i < 10; i++ {
+		order := &pb.Order{
+			OrderId: fmt.Sprintf("ORDER-%d", i),
+			Qty:     int32(i + 5),
+		}
+		if err := stream.Send(order); err != nil {
+			level.Error(r.logger).Log("msg", "Failed to send reply on stream", "error", err)
+			return err
+		}
+	}
+	return nil
 }
 
 func (r *rpcHandler) GetCustomerDetails(stream pb.Customers_GetCustomerDetailsServer) error {
@@ -30,17 +71,17 @@ func (r *rpcHandler) GetCustomerDetails(stream pb.Customers_GetCustomerDetailsSe
 			return err
 		}
 
-		level.Info(r.logger).Log("msg","Received request for customer", "customer_id", customerRequest.CustomerId)
+		level.Info(r.logger).Log("msg", "Received request for customer", "customer_id", customerRequest.CustomerId)
 
 		reply := &pb.Customer{
 			CustomerId: customerRequest.CustomerId,
-			GivenName: "Random",
-			Surname: "Entropy",
+			GivenName:  "Random",
+			Surname:    "Entropy",
 			Address: &pb.Address{
-				City: "Somwewhere",
+				City:  "Somwewhere",
 				Line1: "101 Main St",
 				State: "SW",
-				Zip: "99999",
+				Zip:   "99999",
 			},
 		}
 
